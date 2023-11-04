a61335f1c37bd34ecc515d8cec88bd5a83143f9f
lotfi <lotfibenabdelaziz01@gmail.com>
2023-11-04 22:30:55 +0100

    add mvc

---
+++ apps/api/src/server/modules/search/search.entity.ts
@@ -0,0 +1,13 @@
+// import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm'
+
+// @Entity()
+// export class SearchEntity {
+//   @PrimaryGeneratedColumn()
+//   id: number
+
+//   @Column({ type: 'text' })
+//   searchTerm: string
+
+//   @CreateDateColumn()
+//   createdAt: Date
+// }
--- apps/api/src/server/modules/search/search.service.ts
+++ apps/api/src/server/modules/search/search.service.ts
@@ -1,10 +1,10 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common'
+// import { BadRequestException, Injectable, Logger } from '@nestjs/common'
 
-@Injectable()
-export class SearchService {
-  private logger = new Logger(this.constructor.name)
+// @Injectable()
+// export class SearchService {
+//   private logger = new Logger(this.constructor.name)
 
-  async storeSearchQuery(body: any): Promise<any> {
-    this.logger.log(`Storing search query ${body.organization.name}`)
-  }
-}
+//   async storeSearchQuery (body: any): Promise<any> {
+//     this.logger.log(`Storing search query ${body.organization.name}`)
+//   }
+// }
